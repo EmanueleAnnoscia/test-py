bd5f96efd85a1db84f411f028fdeac597957599f
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-04-23 08:31:08 +0200

    python test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-print("Hello World", "Coglioni!")
+print("Hello World", "Mondo")
 
 print(6 + 6)
 
 fogli= 3
 cartelle= 5
 print(fogli + cartelle)
+
+import webbrowser
+webbrowser.open("https://scryfall.com/sets")
